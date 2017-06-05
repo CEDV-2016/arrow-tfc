ef09d71e7e9d1473d1aae11386d36f314781d186
Jota <josejuanr27@hotmail.com>
2017-06-05 18:29:44 +0200

    [Particles] Create Particle

---
+++ include/entities/Particle.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Ogre.h>
+#include <OgreParticleSystem.h>
+
+class Particle
+{
+public:
+    Particle(std::string name, std::string id, Ogre::Vector3 position);
+    ~Particle();
+
+    void update (Ogre::Real deltaT);
+    void changePosition (Ogre::Vector3 position);
+    void finish ();
+    bool isFinished () { return _isFinished; };
+    void changeDirection (Ogre::Vector3 direction);
+    float getMinVelocity ();
+
+private:
+    Ogre::ParticleSystem *_particle;
+    Ogre::SceneNode *_node;
+    Ogre::SceneManager *_sceneMgr;
+    bool _isFinished;
+    Ogre::Real _timeToFinish;
+    int _id;
+};
---
+++ src/entities/Particle.cpp
@@ -0,0 +1,59 @@
+#include "Particle.hpp"
+#include <limits>
+
+Particle::Particle (std::string name, std::string id, Ogre::Vector3 position)
+: _particle(nullptr), _node(nullptr), _isFinished(false)
+{
+    _sceneMgr = Ogre::Root::getSingletonPtr()->getSceneManager("SceneManager");
+    _particle = _sceneMgr->createParticleSystem("ParticleSystem" + std::to_string(_id), name);
+    _node = _sceneMgr->createSceneNode(id);
+    _node->attachObject(_particle);
+    _node->setPosition(position);
+    _sceneMgr->getRootSceneNode()->addChild(_node);
+
+    if (_particle->getEmitter(0)->getMaxDuration() > 0) {
+        _timeToFinish = _particle->getEmitter(0)->
+            getMaxDuration() + _particle->getEmitter(0)->getMaxTimeToLive();
+    }
+    else {
+        _timeToFinish = std::numeric_limits<float>::max();
+    }
+}
+
+Particle::~Particle ()
+{
+    _particle->detachFromParent();
+    _sceneMgr->destroyParticleSystem(_particle);
+    _sceneMgr->getRootSceneNode()->removeChild(_node);
+    _sceneMgr->destroySceneNode(_node);
+}
+
+void Particle::update (Ogre::Real deltaT)
+{
+    _particle->_update(deltaT);
+    _timeToFinish -= deltaT;
+    if (_timeToFinish < 0) {
+        _isFinished = true;
+    }
+}
+
+void Particle::finish ()
+{
+  _particle->getEmitter(0)->setEmissionRate(0);
+  _timeToFinish = _particle->getEmitter(0)->getMaxTimeToLive();
+}
+
+void Particle::changePosition (Ogre::Vector3 position)
+{
+  _node->setPosition(position);
+}
+
+void Particle::changeDirection (Ogre::Vector3 direction)
+{
+  _particle->getEmitter(0)->setDirection(direction);
+}
+
+float Particle::getMinVelocity ()
+{
+  return _particle->getEmitter(0)->getMinParticleVelocity();
+}
